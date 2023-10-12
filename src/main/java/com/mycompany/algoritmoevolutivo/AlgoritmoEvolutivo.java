be412c740e6902176ab02496c7629d5e477e55b3
Yahir Leo <yahirrocki@outlook.com>
2023-10-11 21:19:04 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/algoritmoevolutivo/AlgoritmoEvolutivo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmoevolutivo;
+
+import Gui.Gui;
+
+/**
+ *
+ * @author Yiro
+ */
+public class AlgoritmoEvolutivo {
+
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+        gui.setVisible(true);
+        gui.setLocationRelativeTo(null);
+    }
+}
